748fb207a94302f496d8cdff79501375c97c25cb
christian <christiandeveloper123@gmail.com>
2024-09-24 15:51:38 -0300

    Salvando mudanças antes da migração para built-in-next-font

--- src/app/components/buttonDarkMode.tsx
+++ src/app/components/buttonDarkMode.tsx
@@ -1,7 +1,7 @@
 export default function ButtonDarkMode(){
     return(
         <>
-            <div className={` top-0 left-0 w-16 pl-[2px] h-8 flex items-center bg-white rounded-full shadow-md transform transition-transform duration-300`}> 
+            <div className={`top-0 left-0 w-16 pl-[2px] h-8 flex items-center bg-white rounded-full shadow-md transform transition-transform duration-300`}> 
                 <div className="bg-PrimaryPurple cursor-pointer h-7 w-7 rounded-full">
                 </div>
             </div>
