383cc1b8dc17884969634e59e06d57a1386bf0e1
christian <christiandeveloper123@gmail.com>
2024-10-21 17:42:56 -0300

    atualização das media querys

--- src/app/pages/Skills.tsx
+++ src/app/pages/Skills.tsx
@@ -40,7 +40,7 @@ export default function Skills() {
         <Element name="Skills">
             <div ref={ref} className="flex h-[100%] mt-52 flex-col items-center w-full justify-center overflow-hidden">
                 <motion.h1
-                    className="text-5xl font-bold text-center dark:text-white max-sm:text-[8vw] "
+                    className="text-5xl mb-10 mt-10 font-bold text-center dark:text-white max-sm:text-[8vw]"
                     initial="hidden"
                     animate={isVisible ? "visible" : "hidden"}
                     variants={{ hidden: { opacity: 0 }, visible: { opacity: 1 } }}
@@ -49,23 +49,36 @@ export default function Skills() {
                     Minhas <span className="text-PrimaryPurple"> habilidades. </span>
                 </motion.h1>
                 <motion.div
-                    className="flex pb-16 flex-wrap justify-center gap-5 mt-10"
+                className='gap-5 flex flex-col'
                     initial="hidden"
                     animate={isVisible ? "visible" : "hidden"}
                     variants={containerVariants}
                     transition={{ duration: 0.5 }}
                 >
-                    <SkillsIcon Icon={DiHtml5} />
-                    <SkillsIcon Icon={IoLogoCss3} />
-                    <SkillsIcon Icon={DiJsBadge} />
-                    <SkillsIcon Icon={SiTypescript} />
-                    <SkillsIcon Icon={DiReact} />
-                    <SkillsIcon Icon={SiNextdotjs} />
-                    <SkillsIcon Icon={SiTailwindcss} />
-                    <SkillsIcon Icon={FaGitAlt} />
-                    <SkillsIcon Icon={IoLogoNodejs} />
-                    <SkillsIcon Icon={SiMysql} />
-                    <SkillsIcon Icon={FaFigma} />
+                    <div className='max-sm:flex gap-5'>
+                        <div className='flex gap-5 max-sm:flex-col'>
+                            <SkillsIcon Icon={DiHtml5} />
+                            <SkillsIcon Icon={IoLogoCss3} />
+                            <SkillsIcon Icon={DiJsBadge} />
+                            <SkillsIcon Icon={SiTypescript} />
+                        </div>
+                        <div className='flex gap-5 max-sm:flex-col sm:mt-5'>
+                            <SkillsIcon Icon={DiReact} />
+                            <SkillsIcon Icon={SiNextdotjs} />
+                            <SkillsIcon Icon={SiTailwindcss} />
+                            <SkillsIcon Icon={FaGitAlt} />
+                        </div>
+                    </div>
+                    <div className='sm:flex gap-5 items-center justify-center'>
+                        <div className='flex gap-5'>
+                            <SkillsIcon Icon={IoLogoNodejs} />
+                            <SkillsIcon Icon={SiMysql} />
+                        </div>
+                        <div className='flex items-center justify-center max-sm:mt-5'>
+                            <SkillsIcon Icon={FaFigma} />
+                        </div>
+                       
+                    </div>
                 </motion.div>
             </div>
         </Element>
